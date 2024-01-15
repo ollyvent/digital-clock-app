6b91700769892acf8197899262c1e5ebbff22441
unknown <diloch111@gmail.com>
2024-01-15 19:37:28 +0000

    create variables to select hour, minutes and seconds and am-pm elements

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// variables to target the hour, minutes, seconds and AM-PM elements
+const hourEl = document.getElementById('hour');
+const minnutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+const amPmEl = document.getElementById('am-pm');
+
